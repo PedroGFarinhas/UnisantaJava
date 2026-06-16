0928fbef7a97bbe421fffea6c30bb1f8f5c45806
Pedro <pedro.gfainhas@senacsp.edu.br>
2026-06-16 11:20:19 -0300

    Initial commit

---
+++ src/programa.java
@@ -0,0 +1,13 @@
+
+public class programa {
+    //botao direito pasta crc new java class
+// digitar psvm e apertar tab
+public static void main() {
+
+    System.out.println("Hello World!");
+
+
+}
+
+
+}
