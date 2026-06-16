e9cca858a99965090c3bf795fb79f14f4993533a
Pedro <pedro.gfainhas@senacsp.edu.br>
2026-06-16 11:34:35 -0300

    primeiro programa soma

--- src/programa.java
+++ src/programa.java
@@ -1,13 +1,23 @@
-
+import java.util.Scanner;
 public class programa {
-    //botao direito pasta crc new java class
-// digitar psvm e apertar tab
-public static void main() {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+
+    //botao direito pasta crc new java class;
+// digitar psvm e apertar tab;
+
+ int a, b, soma;
 
     System.out.println("Hello World!");
 
+        System.out.println("Digite o primeiro valor");
+        a = ler.nextInt();
 
-}
+        System.out.println("Digite o primeiro valor");
+        b = ler.nextInt();
 
-
+        soma = a + b;
+        System.out.println("A soma dos valores é: " + soma);
+    }
 }
