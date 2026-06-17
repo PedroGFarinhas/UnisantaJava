c4d5e4103c741fcbeff3ec58546e47e525795313
Pedro <pedro.gfainhas@senacsp.edu.br>
2026-06-17 10:43:07 -0300

    aula 2

--- src/programa.java
+++ src/programa.java
@@ -4,20 +4,40 @@ public class programa {
         Scanner ler = new Scanner(System.in);
 
 
-    //botao direito pasta crc new java class;
+    //botao direito pasta src new java class;
 // digitar psvm e apertar tab;
 
- int a, b, soma;
+        int a, b, soma;
+        double nota;
+        String nome;
 
     System.out.println("Hello World!");
 
         System.out.println("Digite o primeiro valor");
         a = ler.nextInt();
 
-        System.out.println("Digite o primeiro valor");
+        System.out.println("Digite o segundo valor");
         b = ler.nextInt();
+        ler.nextLine();
 
         soma = a + b;
         System.out.println("A soma dos valores é: " + soma);
+        System.out.printf("A soma dos valores é: %d %n", soma);
+
+
+
+        System.out.println("Digite seu nome: ");
+        nome = ler.nextLine();
+
+        System.out.println("Digite sua nota");
+        nota = ler.nextDouble();
+        System.out.printf("Sua nota foi: %2f %n",nota);
+
+        if (nota > 7){
+            System.out.printf(" %s, você foi aprovado %n", nome);
+        }
+        else{
+            System.out.println("reprovado");
+        }
     }
 }
